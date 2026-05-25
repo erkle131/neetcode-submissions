e1202054827c2cb95e0dc18a04c8278fcb0f0e57
Eric Aranda <eric.p.aranda@intel.com>
2026-05-24 21:28:15 -0700

    Add: python-type-conversion-input - submission-0

---
+++ Python For Beginners/python-type-conversion-input/submission-0.py
@@ -0,0 +1,18 @@
+def read_integer() -> int:
+    integer = int(input())
+    return integer
+
+
+def read_float() -> float:
+    float_n = float(input())
+    return float_n
+
+
+# do not modify below this line
+print(read_integer())
+print(read_integer())
+print(read_integer())
+
+print(read_float())
+print(read_float())
+print(read_float())
