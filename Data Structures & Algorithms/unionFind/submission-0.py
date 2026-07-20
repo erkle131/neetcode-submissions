55a34257e119c9cfcee845f5b35c29d46f2d7aa8
Eric Aranda <eric.p.aranda@intel.com>
2026-07-19 20:41:57 -0700

    Add: unionFind - submission-0

---
+++ Data Structures & Algorithms/unionFind/submission-0.py
@@ -0,0 +1,33 @@
+class UnionFind:
+    
+    def __init__(self, n: int):
+        self.par = [i for i in range(n)]
+        self.rank = [0] * n
+        self.num_components = n
+
+    def find(self, x: int) -> int:
+        if x != self.par[x]:
+            self.par[x] = self.find(self.par[x])
+        return self.par[x]
+
+    def isSameComponent(self, x: int, y: int) -> bool:
+        return self.find(x) == self.find(y)
+
+    def union(self, x: int, y: int) -> bool:
+        p1, p2 = self.find(x), self.find(y)
+        if p1 == p2:
+            return False
+
+        if self.rank[p1] > self.rank[p2]:
+            self.par[p2] = p1
+        elif self.rank[p1] < self.rank[p2]:
+            self.par[p1] = p2
+        else:
+            self.par[p1] = p2
+            self.rank[p2] += 1
+            
+        self.num_components -= 1
+        return True
+
+    def getNumComponents(self) -> int:
+        return self.num_components
